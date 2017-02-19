69e1c445bb8e55aca7d97cb88ab4ee0fe737b73e
Baligh <gasmibal@gmail.com>
2017-02-19 21:30:46 +0100

    First commit.
    Still doing it ... first commit ;)

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 const char* WINDOW_TITLE = "SDL game";
+int gameRunning = 1;
 
 struct button_s {
   const char *name;
@@ -18,10 +19,9 @@ struct button_s {
 
 int main(int argc, char *argv[])
 {
-  int gameRunning = 1;
-  SDL_Surface *screen;
+  SDL_Surface *_pScreen = NULL;
   SDL_Event event;
-  const SDL_VideoInfo* screenInfo = NULL;
+  const SDL_VideoInfo* _pScreenInfo = NULL;
   int screenWidth = WINDOW_WIDTH;
   int screenHeight = WINDOW_HEIGHT;
 
@@ -40,13 +40,22 @@ int main(int argc, char *argv[])
   SDL_WM_SetCaption(WINDOW_TITLE, NULL);
   //SDL_WM_SetIcon(SDL_LoadBMP(icon),NULL);
 
-  screenInfo = SDL_GetVideoInfo();
-  screenWidth = screenInfo->current_w;
-  screenHeight = screenInfo->current_h;
+  _pScreenInfo = SDL_GetVideoInfo();
+  screenWidth = _pScreenInfo->current_w;
+  screenHeight = _pScreenInfo->current_h;
   
-  screen = SDL_SetVideoMode( 0 /*screenWidth*/, 0 /*screenHeight*/, 0, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
-  if (screen == NULL) {
+  _pScreen = SDL_SetVideoMode(screenWidth, screenHeight, 0, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
+  if (_pScreen == NULL) {
     log_error("Error creating window");
+    TTF_Quit();
+    SDL_Quit();
+    return 128;
+  }
+
+  log_info("FullScreen: Yes, Screen Width: %d, Screen Height: %d", screenWidth, screenHeight);
+
+  if (Menu_Init() != 0) {
+    log_error("Menu initialization");
     TTF_Quit();
     SDL_Quit();
     return 128;
@@ -59,10 +68,16 @@ int main(int argc, char *argv[])
       }
     }
 
-    menu_init(screen);
+    Menu_Action(&event);
 
-    SDL_Flip(screen);
+    Menu_Blit(_pScreen);
+
+    SDL_Flip(_pScreen);
   }
+
+  Menu_Deinit();
+
+  SDL_FreeSurface(_pScreen);
 
   TTF_Quit();
   SDL_Quit();
--- src/menu.c
+++ src/menu.c
@@ -11,86 +11,215 @@
 #define SCREEN_BPP 32
 
 #define TTF_FONT_FILENAME "img/Roboto-Italic.ttf"
-#define TTF_FONT_SIZE 64
+#define TTF_FONT_SIZE 58
 
 #define IMG_BACKGROUND_FILENAME "img/background.png"
 #define IMG_BUTTON_FILENAME "img/button.png"
-#define IMG_BTN1_FILENAME "play.png"
-#define IMG_BTN2_FILENAME "multiplayer.png"
-#define IMG_BTN3_FILENAME "option.png"
-#define IMG_BTN4_FILENAME "quit.png" 
 
-#define MIX_EF_FILENAME "switch.wav"
-#define MIX_BACK_FILENAME "back.mp3"
-
-struct menu_s {
-  SDL_Surface *menuSurface;
-  TTF_Font *font;
+struct button_s {
+  const char *label;
+  SDL_Surface *s;
+  SDL_Rect p;
+  int (*button_action)(void);
 };
 
-SDL_Surface *menu_init(SDL_Surface *pScreen)
+struct page_s {
+  SDL_Surface *s;
+};
+
+struct menu_s {
+  SDL_Surface *bg;
+  SDL_Surface *button;
+  TTF_Font *font;
+  int idxButton;
+};
+
+extern int gameRunning;
+
+static struct menu_s gMenu = {
+  NULL,
+  NULL,
+  NULL,
+  0,
+};
+
+static int play_action(void)
 {
-  SDL_Surface *_pOrigMenu = NULL;
-  SDL_Surface *_pMenu = NULL;
-
-  _pOrigMenu = IMG_Load(IMG_BACKGROUND_FILENAME);
-  if (_pOrigMenu == NULL) {
-    log_error();
-    return NULL;
-  }
-
-  _pMenu = SDL_ConvertSurface(_pOrigMenu, pScreen->format, 0);
-  if (_pMenu == NULL) {
-    log_error();
-    return NULL;
-  } 
-
-  SDL_FreeSurface(_pOrigMenu);
-
-  SDL_BlitSurface(_pMenu, NULL, pScreen, NULL);
-
-  menu_addButton(pScreen, "Play");
-
-  //SDL_BlitSurface(_pMenu, NULL, pScreen, NULL);
-
-  return _pMenu;
+  log_info("Play game");
+  return 0;
 }
 
-int menu_addButton(SDL_Surface *pMenu, const char *label)
+static int options_action(void)
 {
-  SDL_Surface *_pButton = NULL;
-  TTF_Font *_pFont = NULL;
-  SDL_Surface *_pText = NULL;
-  SDL_Color blackColor = {0};
-  SDL_Rect offset;
+  log_info("Option page to load ...");
+  return 0;
+}
 
-  _pButton = IMG_Load(IMG_BUTTON_FILENAME);
-  if (_pButton == NULL) {
-    log_error();
+static int quit_action(void)
+{
+  log_info("QUIT GAME: BYE BYE");
+  gameRunning = 0;
+  return 0;
+}
+
+static struct button_s gButtons[] = {
+  {"play", NULL, {0}, play_action},
+  {"multiplayer", NULL, {0}, NULL},
+  {"options", NULL, {0}, options_action},
+  {"quit", NULL, {0}, quit_action},
+};
+
+int Menu_Init()
+{
+  gMenu.bg = IMG_Load(IMG_BACKGROUND_FILENAME);
+  if (gMenu.bg == NULL) {
+    log_error("Loading background: %s", IMG_GetError());
     return 1;
   }
 
-  _pFont = TTF_OpenFont(TTF_FONT_FILENAME, TTF_FONT_SIZE);
-  if (_pFont == NULL) {
+  gMenu.button = IMG_Load(IMG_BUTTON_FILENAME);
+  if (gMenu.button == NULL) {
+    log_error("Loading button: %s", IMG_GetError());
+    SDL_FreeSurface(gMenu.bg);
+    gMenu.bg = NULL;
+    return 1;
+  }
+
+  gMenu.font = TTF_OpenFont(TTF_FONT_FILENAME, TTF_FONT_SIZE);
+  if (gMenu.font == NULL) {
+    SDL_FreeSurface(gMenu.bg);
+    gMenu.bg = NULL;
     log_error("Loading font file(%s):[%s]", TTF_FONT_FILENAME, TTF_GetError());
     return 1;
   }
 
-  _pText = TTF_RenderText_Solid(_pFont, label, blackColor);
-  if (_pText == NULL) {
-    log_error();
+  return 0;
+}
+
+int Menu_Deinit()
+{
+  int _i = 0;
+  int buttonNbr = sizeof(gButtons)/sizeof(gButtons[0]);
+
+  SDL_FreeSurface(gMenu.bg);
+  SDL_FreeSurface(gMenu.button);
+  TTF_CloseFont(gMenu.font);
+  
+  for (_i=0; _i<buttonNbr; ++_i) {
+    SDL_FreeSurface(gButtons[_i].s);
+  }
+
+  return 0;
+}
+
+int Menu_Action(SDL_Event *pEvent)
+{
+  static int lastEventType = -1;
+  int _i = 0;
+  int buttonNbr = sizeof(gButtons)/sizeof(gButtons[0]);
+
+  /* keyboard handling */
+  if ((pEvent->type == SDL_KEYDOWN) && (lastEventType != SDL_KEYDOWN)) {
+    if (pEvent->key.keysym.sym == SDLK_DOWN) {
+      gMenu.idxButton = ++gMenu.idxButton % buttonNbr;
+    }
+    if (pEvent->key.keysym.sym == SDLK_UP) {
+      if (--gMenu.idxButton < 0) gMenu.idxButton += buttonNbr;
+    }
+    lastEventType = SDL_KEYDOWN;
+  }
+  if ((pEvent->type == SDL_KEYUP) && (lastEventType != SDL_KEYUP)) {
+    lastEventType = SDL_KEYUP;
+    /* Enter key: Action on button */
+    if (pEvent->key.keysym.sym == SDLK_RETURN) {
+      if (gButtons[gMenu.idxButton].button_action) {
+        gButtons[gMenu.idxButton].button_action();
+      }
+    }
+  }
+
+  /* Mouse handling */
+  if ((pEvent->type == SDL_MOUSEMOTION) || (pEvent->type == SDL_MOUSEBUTTONUP)) {
+    for (_i = 0; _i < buttonNbr; ++_i) {
+      if ((pEvent->motion.x > gButtons[_i].p.x ) && (pEvent->motion.x < gButtons[_i].p.x + gButtons[_i].p.w ) && (pEvent->motion.y > gButtons[_i].p.y ) && (pEvent->motion.y < gButtons[_i].p.y + gButtons[_i].p.h )) {
+        gMenu.idxButton = _i;
+
+        if (pEvent->motion.state == SDL_PRESSED) {
+          if (gButtons[gMenu.idxButton].button_action) {
+            gButtons[gMenu.idxButton].button_action();
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
+
+int Menu_Blit(SDL_Surface *pDst)
+{
+  if (SDL_BlitSurface(gMenu.bg, NULL, pDst, NULL) != 0) {
+    log_error("Bliting background image: %s", SDL_GetError());
     return 1;
   }
 
-  offset.x = (_pButton->w / 2) - (_pText->w / 2);
-  offset.y = (_pButton->h / 2) - (_pText->h / 2);
-  SDL_BlitSurface(_pText, NULL, _pButton, &offset);
+  if (Menu_AddButtons(pDst) != 0) {
+    log_error("Adding buttons to menu");
+    return 1;
+  }
 
-  offset.x = (pMenu->w / 2) - (_pButton->w / 2);
-  offset.y = (pMenu->h / 2) - (_pButton->h / 2);
-  SDL_BlitSurface(_pButton, NULL, pMenu, &offset);
+  return 0;
+}
 
-  TTF_CloseFont(_pFont);
+int Menu_AddButtons(SDL_Surface *pDst)
+{
+  SDL_Surface *_pText = NULL;
+  SDL_Color blackColor = {0};
+  SDL_Color selectedColor = {255, 255, 128, 0};
+  int _i = 0;
+  int buttonNbr = sizeof(gButtons)/sizeof(gButtons[0]);
+  SDL_Rect offset;
+
+  if (gMenu.button == NULL) {
+    log_error("Menu not initialized");
+    return 1;
+  }
+  
+  for(_i=0; _i<buttonNbr; ++_i) {
+    /* Free old surface */
+    if (gButtons[_i].s != NULL) {
+      SDL_FreeSurface(gButtons[_i].s);
+      gButtons[_i].s = NULL;
+    }
+    /* Copy new surface */
+    gButtons[_i].s = SDL_ConvertSurface(gMenu.button, gMenu.button->format, SDL_HWSURFACE);
+    if (gButtons[_i].s == NULL) {
+      log_error("Copy the loaded image: %s", SDL_GetError());
+      return 1;
+    }
+    /* render label */
+    _pText = TTF_RenderText_Solid(gMenu.font, gButtons[_i].label, (gMenu.idxButton == _i) ? selectedColor : blackColor);
+    if (_pText == NULL) {
+      log_error("Font loading: %s", TTF_GetError());
+      SDL_FreeSurface(gButtons[_i].s);
+      return 1;
+    }
+    /* Set Text in the correct postion */
+    offset.x = (gButtons[_i].s->w / 2) - (_pText->w / 2);
+    offset.y = (gButtons[_i].s->h / 2) - (_pText->h / 2);
+    SDL_BlitSurface(_pText, NULL, gButtons[_i].s, &offset);
+  }
+
+  /* offset of all buttons (center) */
+  offset.x = (pDst->w / 2) - (gButtons[0].s->w / 2);
+  int _y = (pDst->h / 2) - ((gButtons[0].s->h + gButtons[0].s->h/4) * (buttonNbr-1) / 2);
+
+  for (_i=0; _i<buttonNbr; ++_i) {
+    gButtons[_i].p.x = offset.x;
+    gButtons[_i].p.y = _y + (gButtons[_i].s->h + gButtons[_i].s->h/4) * _i;
+    SDL_BlitSurface(gButtons[_i].s, NULL, pDst, &gButtons[_i].p);
+  }
+
   return 0;
 }
 
